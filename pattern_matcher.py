365a77a15860845959b41abb57fdabc73427fe6a
Nick Porter <worldofnick@users.noreply.github.com>
2017-10-30 14:50:11 -0600

    is_passive

--- pattern_matcher.py
+++ pattern_matcher.py
@@ -1,6 +1,8 @@
-import ntlk
+import nltk
+import numpy as np
 from nltk import word_tokenize, pos_tag, ne_chunk
 
+# Returns true if the sentence is in passive voice
 def is_passive(tagged_sentence):
     passive_tags = [['VBZ', 'VBN'], ['VBP', 'VBN'],
     ['VBZ', 'VBG', 'VBN'], ['VBP', 'VBG', 'VBN'],
@@ -11,6 +13,15 @@ def is_passive(tagged_sentence):
     ['MD', 'VB', 'VBN'],
     ['MD', 'VB', 'VBN', 'VBN']]
 
+    tags = [word[1] for word in tagged_sentence]
+
+    tags = np.array(tags)
+    passive_tags = np.array(passive_tags)
+
+    for passive_tag in passive_tags:
+        if any((passive_tag == tags).all() for tags in passive_tag):
+            return True
+
     return False
 
 
@@ -20,4 +31,4 @@ def find_matches(text):
     perp = None
     victim = None
     for sentence in sent_text:
-        tagged_sentence = ne_chunk(pos_tag(word_tokenize(sentence)))
+        tagged_sentence = pos_tag(word_tokenize(sentence))
