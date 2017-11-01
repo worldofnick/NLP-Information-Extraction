e5db659eb9c47311f7304fe201dd07e8185b790d
Nick Porter <worldofnick@users.noreply.github.com>
2017-10-31 20:30:25 -0600

    Strip article

--- Article.py
+++ Article.py
@@ -1,14 +1,8 @@
 # Holds the text and the ID to the article.
-
+from __future__ import unicode_literals
 class Article:
     def __init__(self, text):
-        self.TEXT_MARKER = '[TEXT]'
         self.id = text[0].split()[0]
 
         text.pop(0)
-        # for i in range(0, len(text)):
-        #     text[i] = text[i].strip()
-        #
         self.text = ' '.join(text)
-        # date_index = self.text.index(self.TEXT_MARKER)
-        # self.text = self.text[date_index + len(self.TEXT_MARKER) + 1:]
