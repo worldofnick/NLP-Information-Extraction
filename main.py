fdc07d6bfa14a3a7ef07e8a4ead927ae64825ce1
Nick Porter <worldofnick@users.noreply.github.com>
2017-11-01 19:36:06 -0600

    write to file

--- main.py
+++ main.py
@@ -33,10 +33,12 @@ def read_weapons():
     print weapons
 
 def main():
+    file = open('testfile.txt','w')
     for file_name in os.listdir(ARTICLE_LOCATION):
         text = open(os.path.join(os.path.dirname(__file__), ARTICLE_LOCATION + '/' + file_name)).readlines()
         article = Article(text)
         extracted_info = classify(article, load_text('weapons.txt'), load_text('killingverbs.txt'))
+        extracted_info.write_template(file)
 
 
 main()
