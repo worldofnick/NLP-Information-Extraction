85adb71e8a8b345a93a4685f6111db376ec11179
Nick Porter <worldofnick@users.noreply.github.com>
2017-11-01 22:51:12 -0600

    find orgs

--- classifier.py
+++ classifier.py
@@ -3,11 +3,11 @@ from ExtractedInfo import ExtractedInfo
 from classify_incident import classify_inci
 from detect_weapons import extract_weapons
 from pattern_matcher import find_victims
+from detect_org import detect_orgs
 
 def classify(article, common_weapons, killing_verbs):
     incident_type = classify_inci(article.text)
     weapons = extract_weapons(article.text, common_weapons)
-    victims = find_victims(article.text, killing_verbs)
+    orgs = detect_orgs(article.text)
 
-    extracted_info = ExtractedInfo(article.id, incident_type, weapons, [], [], [], [])
-    return extracted_info
+    return ExtractedInfo(article.id, incident_type, weapons, [], orgs, [], [])
