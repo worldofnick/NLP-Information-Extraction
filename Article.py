6db053bb93c6eebf716ac5434ba9d7220591f484
Nick Porter <worldofnick@users.noreply.github.com>
2017-11-03 12:44:27 -0600

    Remove print line

--- Article.py
+++ Article.py
@@ -3,6 +3,5 @@ from __future__ import unicode_literals
 class Article:
     def __init__(self, text):
         self.id = text.split()[0]
-        print self.id
         text = text[:len(self.id)]
         self.text = ' '.join(text)
