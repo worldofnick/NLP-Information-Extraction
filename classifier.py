ae5248571ccea67ee6a481b13c57e10a3d33fded
Nick Porter <worldofnick@users.noreply.github.com>
2017-11-04 17:56:00 -0600

    detect victims

--- classifier.py
+++ classifier.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 from ExtractedInfo import ExtractedInfo
 from classify_incident import classify_inci
 from detect_weapons import extract_weapons
-from pattern_matcher import find_victims
+from detect_victims import detect_victims
 from detect_org import detect_orgs
 from detect_perp_individual import detect_individuals
 from detect_targets import detect_targets
@@ -12,10 +12,7 @@ def classify(article, common_weapons, killing_verbs, en_nlp):
     weapons = extract_weapons(article.text, common_weapons)
     orgs = detect_orgs(article.text, killing_verbs, en_nlp)
     targets = detect_targets(article.text, killing_verbs, en_nlp)
-    # victims = find_victims(article.text, killing_verbs, en_nlp)
+    victims = detect_victims(article.text)
     individuals = detect_individuals(article.text, killing_verbs, en_nlp)
 
-    # return ExtractedInfo(article.id, incident_type, weapons, [], [], [], victims)
-    return ExtractedInfo(article.id, incident_type, weapons, individuals, orgs, targets, [])
-    # return ExtractedInfo(article.id, incident_type, weapons, [], [], [], victims)
-    # return ExtractedInfo(article.id, "-", [], [], [], [], victims)
+    return ExtractedInfo(article.id, incident_type, weapons, individuals, orgs, targets, victims)
