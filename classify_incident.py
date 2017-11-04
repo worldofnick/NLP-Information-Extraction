52caa4c49587204b88e2e6f3514f3456a5bcde3a
Nick Porter <worldofnick@users.noreply.github.com>
2017-11-04 17:37:55 -0600

    Improve by 1%

--- classify_incident.py
+++ classify_incident.py
@@ -5,7 +5,7 @@ import re
 arson_dict = {'arson': 4, 'arson(?=\s)': 1, 'houses\s+on\s+fire': 4, 'places\s+burned': 4, 'burning': 4, 'set\s+ablaze': 3, 'set\s+fire\s+to': 3,
 'fires': 1}
 
-attack_dict = {'(dynamite|bomb|bombing)\s+attack': 1, '(?<=\s)attack': 1, '(?<=\s)murder': 1, '(?<=\s)machine gun\s+attack': 3,
+attack_dict = { '(?<=\s)attack': 1, '(?<=\s)murder': 1, '(?<=\s)machine gun\s+attack': 3,
 '(?<=\s)mortar': 3, '(?<=\s)shoot': 2, '(?<=\s)shot': 1, '(?<=\s)ARTILLERY\s+FIRE': 2, '(?<=\s)shoot\-out': 2,
 '(?<=\s)firing\s+rockets': 3, '(?<=\s)launched\s+rockets': 3, '(?<=\s)fired\s+rocket': 2, '(?<=\s)firing': 1,
 '(?<=\s)molotov': 4, '(?<=\s)assassinat': 2, '(?<=\s)massacre': 1}
@@ -17,8 +17,7 @@ bomb_dict = {'(?<=\s)bomb': 1, '(?<=\s)bombed rebel': 1, '(?<=\s)detonated': 5, 
 
 kidnap_dict = {'(?<=\s)kidnap': 4, '(?<=\s)abduct': 4, 'held\s+hostage': 4, 'hostage': 4, '(?<=\s)disappear': 1}
 
-robbery_dict = {'(?<=\s)stole(?=\s)': 2, '(?<=\s)steal': 2, '(?<=\s)robbery(?=\s)': 4, '(?<=\s)robbed(?=\s)': 4,
-'(?<=\s)loot': 4, '(?<=\s)burglar': 3, '(?<=\s)burglary': 4, '(?<=\s)theif': 3, '(?<=\s)raider': 2, '(?<=\s)intruder': 2}
+robbery_dict = {'(?<=\s)mugged': 1, '(?<=\s)stole(?=\s)': 1, '(?<=\s)steal': 1, '(?<=\s)robbery(?=\s)': 1, '(?<=\s)robbed(?=\s)': 1, '(?<=\s)burglar': 1, '(?<=\s)burglary': 1}
 
 def score(text, dict):
     score = 0
@@ -35,5 +34,4 @@ def classify_inci(text):
     score_dict['BOMBING'] = score(text, bomb_dict)
     score_dict['KIDNAPPING'] = score(text, kidnap_dict)
     score_dict['ROBBERY'] = score(text, robbery_dict)
-    score_dict['ROBBERY'] = 0
     return max(score_dict.iteritems(), key=operator.itemgetter(1))[0]
