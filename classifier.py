404e8cc52825a66cd46b8dc5f5c6d94c9387fb97
Nick Porter <worldofnick@users.noreply.github.com>
2017-11-05 13:03:56 -0700

    Inject cased text

--- classifier.py
+++ classifier.py
@@ -11,8 +11,8 @@ def classify(article, common_weapons, killing_verbs, orgs, en_nlp):
     incident_type = classify_inci(article.text)
     weapons = extract_weapons(article.text, common_weapons)
     orgs = detect_org_2(article.text, orgs)
-    targets = detect_targets(article.text, killing_verbs, en_nlp)
-    victims = detect_victims(article.text)
-    individuals = detect_individuals(article.text, killing_verbs, en_nlp)
+    targets = detect_targets(article.cased_text, killing_verbs, en_nlp)
+    victims = detect_victims(article.cased_text)
+    individuals = detect_individuals(article.cased_text, killing_verbs, en_nlp)
 
     return ExtractedInfo(article.id, incident_type, weapons, individuals, orgs, targets, victims)
